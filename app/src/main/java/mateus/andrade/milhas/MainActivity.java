ad98436fa5bf285a9a43e24aa56e5dc756165be8
Mateus Andrade <mateusdoandrade@gmail.com>
2018-01-01 18:30:58 -0200

    Orientation just portrait Scren one fixed some details

--- app/src/main/java/mateus/andrade/milhas/MainActivity.java
+++ app/src/main/java/mateus/andrade/milhas/MainActivity.java
@@ -1,7 +1,11 @@
 package mateus.andrade.milhas;
 
+import android.content.pm.ActivityInfo;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+            //Trocar cor da status bar
+            if (Build.VERSION.SDK_INT >= 21) {
+                Window window = getWindow();
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.setStatusBarColor(getResources().getColor(R.color.topaz));
+            }
+
+
     }
 }
